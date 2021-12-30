fd0628818fa7c4c5dba2fc8af0621ad1b2b6dd58
Jingyi-Guo <83747036+Jingyi-Guo@users.noreply.github.com>
2021-12-30 18:06:48 -0500

    commit

--- app/src/main/java/com/AIOK/app/Signup.java
+++ app/src/main/java/com/AIOK/app/Signup.java
@@ -1,2 +1,42 @@
-package com.AIOK.app;public class Signup {
+package com.AIOK.app;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                //Starting Write and Read data with URL
+                //Creating array for parameters
+                String[] field = new String[2];
+                field[0] = "param-1";
+                field[1] = "param-2";
+                //Creating array for data
+                String[] data = new String[2];
+                data[0] = "data-1";
+                data[1] = "data-2";
+                PutData putData = new PutData("https://projects.vishnusivadas.com/AdvancedHttpURLConnection/putDataTest.php", "POST", field, data);
+                if (putData.startPut()) {
+                    if (putData.onComplete()) {
+                        String result = putData.getResult();
+                        //End ProgressBar (Set visibility to GONE)
+                    }
+                }
+                //End Write and Read data with URL
+            }
+        });
+    }
 }
